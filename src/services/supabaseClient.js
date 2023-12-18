c0ebe8960022f6973a0d7f51b4b1081854a6c8e6
Valerii Rudyka <val.rudika@gmail.com>
2023-12-18 16:13:20 +0200

    Light changes

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-SUPABASE_KEY =
+const SUPABASE_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuZ2NnYnpodnNlaGR0dmNtemVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ5NzcwMjQsImV4cCI6MjAxMDU1MzAyNH0.-n8Wp4dm87sIQ3GAiHEk88P0BXT56kF3SYStjUJvOt0';
 export const supabaseUrl = 'https://kngcgbzhvsehdtvcmzec.supabase.co';
 const supabaseKey = SUPABASE_KEY;
