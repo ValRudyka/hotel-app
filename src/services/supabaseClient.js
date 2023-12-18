f2474128a7935efe111ed580941292cb6be5b3b0
Valerii Rudyka <val.rudika@gmail.com>
2023-12-18 16:12:49 +0200

    Added key

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,7 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = "https://kngcgbzhvsehdtvcmzec.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuZ2NnYnpodnNlaGR0dmNtemVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ5NzcwMjQsImV4cCI6MjAxMDU1MzAyNH0.-n8Wp4dm87sIQ3GAiHEk88P0BXT56kF3SYStjUJvOt0';
+export const supabaseUrl = 'https://kngcgbzhvsehdtvcmzec.supabase.co';
+const supabaseKey = SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
